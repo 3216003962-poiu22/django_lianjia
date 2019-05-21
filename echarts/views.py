23763e78859b6ba5cdce51d047c83571a99e7265
zjx424 <380435428@qq.com>
2019-05-21 12:07:26 +0800

    add_all_city

--- echarts/views.py
+++ echarts/views.py
@@ -2,16 +2,16 @@
 import pandas as pd
 from django.shortcuts import render
 from echarts.number_count_views import index as number_count_views_index
-from echarts.city_info import city_short_name
+from echarts.city_info import city_short_name, make_city_dict
+
 
 def mainpage(request):
-    return render(request, 'mainpage/mainpage.html')
 
+    city_dict = make_city_dict()
+    return render(request, 'mainpage/mainpage.html', {"city_dict": city_dict})
 
 
 def index(request, city):
-    city_short_name_list=city_short_name()
+    city_short_name_list = city_short_name()
     if city in city_short_name_list:
         return number_count_views_index(request, city)
-
-
